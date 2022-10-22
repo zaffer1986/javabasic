344dd60a88157aeb1fa32c4e03aa58c33ca9b158
zaffer1986 <112616729+zaffer1986@users.noreply.github.com>
2022-10-22 10:46:18 +0200

    lesson

--- src/hw9/Cat1.java
+++ src/hw9/Cat1.java
@@ -1,9 +1,11 @@
 public class Cat1 extends Animal implements IAnimal {
     public Cat1(String name, String color, int age) {
+
         super(name, color, age);
     }
         @Override
         public String voice() {
-            return "miaw";
+
+        return "miaw";
         }
 }
